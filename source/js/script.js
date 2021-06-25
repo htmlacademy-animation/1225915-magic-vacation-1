1454e73f5aacb01c8161c3a1c59d598716ad3e62
Anton Vavilov <vavilovonline@gmail.com>
2021-06-25 15:32:21 +0300

    added transitions to header and footer elements.

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import onLoadBody from './modules/on-load-body';
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
@@ -21,4 +22,4 @@ chat();
 result();
 form();
 social();
-
+onLoadBody();
